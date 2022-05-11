b2ad72632c5e4aa751d10a56b2ef52b19b2143d8
Aryan <aryan.gupta_cs19@gla.ac.in>
2022-05-11 18:11:22 +0530

    added profile update notification

--- core/customer/views.py
+++ core/customer/views.py
@@ -1,9 +1,10 @@
 from audioop import reverse
+import imp
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from core.customer import forms
 from django.urls import reverse
-
+from django.contrib import messages
 # Create your views here.
 
 @login_required()
@@ -22,6 +23,8 @@ def profile_page(request):
         if user_form.is_valid() and customer_form.is_valid():
             user_form.save()
             customer_form.save()
+
+            messages.success(request, "Your profile has been updated")
             return redirect(reverse('customer:profile'))
 
     return render(request, 'customer/profile.html', {
